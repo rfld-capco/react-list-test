1991af53345cf080f3022000fb071169067f67e8
rfld-capco <Rory.fielding@capco.com>
2019-08-20 17:13:32 +0100

    Added forms

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -3,10 +3,11 @@ import Contact from './Contact';
 
 class ContactsList extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            search: ""
+            search: "",
+            contacts: props.contacts
         }
     }
 
@@ -17,8 +18,23 @@ class ContactsList extends React.Component {
         });
     }
 
+    addContact(event) {
+        //Stops the whole page from refreshing
+        event.preventDefault();
+        let name = this.refs.name.value;
+        let phone = this.refs.phone.value;
+        let id = Math.floor((Math.random() * 100 + 1))
+        this.setState({
+            contacts: this.state.contacts.concat({
+                id, name, phone
+            })
+        })
+        this.refs.name.value = '';
+        this.refs.phone.value = '';
+    }
+
     render() {
-        let filteredContacts = this.props.contacts.filter(
+        let filteredContacts = this.state.contacts.filter(
             (contact) => {
                 return contact.name.toLowerCase().indexOf(this.state.search) !== -1;
             }
@@ -26,8 +42,14 @@ class ContactsList extends React.Component {
         return (
             <div>
                 <input type="text"
+                    placeholder="Search"
                     value={this.state.search}
                     onChange={this.updateSearch.bind(this)} />
+                <form onSubmit={this.addContact.bind(this)}>
+                    <input type="text" ref="name" />
+                    <input type="text" ref="phone" />
+                    <button type="submit">Add New Contact</button>
+                </form>
                 <ul>
                     {filteredContacts.map((contact) => {
                         return <Contact contact={contact} key={contact.id} />
